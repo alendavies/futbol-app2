c790f04745d07f335cca640903db9e3324d28892
alendavies <alendavies10@gmail.com>
2023-12-23 16:09:50 -0300

    navbar y layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            <Navbar />
+            {children}
+        </div>
+    );
+}
+
+export default Layout;
